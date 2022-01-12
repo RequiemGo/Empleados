f8f93e2db574221666fa4b045ac43a1036ed4810
Andres Castillo <castilloreyesandres@gmail.com>
2022-01-12 15:49:08 -0500

    add south

--- empleado/settings/local.py
+++ empleado/settings/local.py
@@ -17,16 +17,16 @@ ALLOWED_HOSTS = []
 #         'PORT': '5432',
 #     }
 # }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'd2td91ebvm79ck',
-#         'USER': 'tgvnbylrqgyspl',
-#         'PASSWORD': '7a26e847e643608037a3b84833f61dbe4eda8dfcefbeca0d1bbf7ff78ae6d69a',
-#         'HOST': 'ec2-3-227-15-75.compute-1.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'd2td91ebvm79ck',
+        'USER': 'tgvnbylrqgyspl',
+        'PASSWORD': '7a26e847e643608037a3b84833f61dbe4eda8dfcefbeca0d1bbf7ff78ae6d69a',
+        'HOST': 'ec2-3-227-15-75.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
